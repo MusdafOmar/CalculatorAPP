10505ef8426cae9c7ac4b61b5c7a402d7497b004
MusdafOmar <mustafomar100@gmail.com>
2026-01-07 20:51:18 +0100

    Add Subtract method

--- CalculatorAPP/Calculator.cs
+++ CalculatorAPP/Calculator.cs
@@ -1,10 +1,13 @@
-﻿namespace CalculatorAPP
+﻿public class Calculator
 {
-    public class Calculator
+    public int Add(int a, int b)
     {
-        public int Add(int a, int b)
-        {
-            return a + b;
-        }
+        return a + b;
+    }
+
+    public int Subtract(int a, int b)
+    {
+        return a - b;
     }
 }
+
